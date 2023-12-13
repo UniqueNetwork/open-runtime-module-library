52ab1c045d98b0b3047807ad9aed070d3ebcb950
Daniel Shiposha <mrshiposha@gmail.com>
2023-12-13 17:50:54 +0100

    fix: add DerivativeCollectionData

--- nft/src/xnft.rs
+++ nft/src/xnft.rs
@@ -14,8 +14,8 @@ use xcm::v3::prelude::*;
 use pallet_xnft::traits::{DerivativeWithdrawal, IntoXcmError, NftInterface, PalletError};
 
 use crate::{Config, Error, Pallet};
-pub struct XnftAdapter<T, CollectionId, TokenId, DerivativeClassData, DerivativeTokenData>(
-	PhantomData<(T, CollectionId, TokenId, DerivativeClassData, DerivativeTokenData)>,
+pub struct XnftAdapter<T, CollectionId, TokenId, DerivativeTokenData>(
+	PhantomData<(T, CollectionId, TokenId, DerivativeTokenData)>,
 )
 where
 	T: Config,
@@ -23,26 +23,28 @@ where
 		Deref<Target = T::ClassId> + From<T::ClassId> + Member + Parameter + MaxEncodedLen + TryFrom<Junction>,
 	TokenId:
 		Deref<Target = T::TokenId> + From<T::TokenId> + Member + Parameter + MaxEncodedLen + TryFrom<AssetInstance>,
-	DerivativeClassData: Get<T::ClassData>,
 	DerivativeTokenData: Get<T::TokenData>;
 
-impl<T, CollectionId, TokenId, DerivativeClassData, DerivativeTokenData> NftInterface<T>
-	for XnftAdapter<T, CollectionId, TokenId, DerivativeClassData, DerivativeTokenData>
+impl<T, CollectionId, TokenId, DerivativeTokenData> NftInterface<T>
+	for XnftAdapter<T, CollectionId, TokenId, DerivativeTokenData>
 where
 	T: Config,
 	CollectionId:
 		Deref<Target = T::ClassId> + From<T::ClassId> + Member + Parameter + MaxEncodedLen + TryFrom<Junction>,
 	TokenId:
 		Deref<Target = T::TokenId> + From<T::TokenId> + Member + Parameter + MaxEncodedLen + TryFrom<AssetInstance>,
-	DerivativeClassData: Get<T::ClassData>,
 	DerivativeTokenData: Get<T::TokenData>,
 {
 	type CollectionId = CollectionId;
 	type TokenId = TokenId;
 	type PalletDispatchErrors = Error<T>;
+	type DerivativeCollectionData = T::ClassData;
 
-	fn create_derivative_collection(owner: &T::AccountId) -> Result<Self::CollectionId, DispatchError> {
-		<Pallet<T>>::create_class(owner, vec![], DerivativeClassData::get()).map(Into::into)
+	fn create_derivative_collection(
+		owner: &T::AccountId,
+		class_data: Self::DerivativeCollectionData,
+	) -> Result<Self::CollectionId, DispatchError> {
+		<Pallet<T>>::create_class(owner, vec![], class_data).map(Into::into)
 	}
 
 	fn mint_derivative(
